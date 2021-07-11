7d791754e9b63349a0b577899028d71266bc8516
Patrick Gazaneo <pck1993@gmail.com>
2021-07-11 03:08:04 -0300

    sign in form validation

--- ui/src/views/SignIn.vue
+++ ui/src/views/SignIn.vue
@@ -9,8 +9,18 @@
           type="email"
           id="emailInput"
           class="form-control"
-          v-model="userCredentials.email"
+          v-model.trim="$v.userCredentials.email.$model"
+          :class="{ 'is-invalid': $v.userCredentials.email.$error }"
         />
+        <div
+          v-if="
+            !$v.userCredentials.email.$required ||
+            !$v.userCredentials.email.$email
+          "
+          class="invalid-feedback"
+        >
+          Invalid email
+        </div>
       </div>
       <div class="mb-3">
         <label for="passwordInput" class="form-label">Password</label>
@@ -18,13 +28,24 @@
           type="password"
           id="passwordInput"
           class="form-control"
-          v-model="userCredentials.password"
+          v-model.trim="$v.userCredentials.password.$model"
+          :class="{ 'is-invalid': $v.userCredentials.password.$error }"
         />
+        <div
+          v-if="!$v.userCredentials.password.$required"
+          class="invalid-feedback"
+        >
+          Password is required
+        </div>
+      </div>
+      <div v-if="serverErrorMessage" class="server-error-message">
+        {{ serverErrorMessage }}
       </div>
       <button
         type="button"
         class="w-100 btn btn-primary"
         @click="onSignInHandler"
+        :disabled="$v.$anyDirty && $v.$invalid"
       >
         Sign In
       </button>
@@ -35,6 +56,7 @@
 <script>
 import authService from "../services/auth.service";
 import sessionService from "../services/session.service";
+import { required, email } from "vuelidate/lib/validators";
 
 export default {
   name: "SignIn",
@@ -44,16 +66,38 @@ export default {
         email: "",
         password: "",
       },
+      serverErrorMessage: "",
     };
+  },
+  validations: {
+    userCredentials: {
+      email: {
+        required,
+        email,
+      },
+      password: {
+        required,
+      },
+    },
   },
   methods: {
     async onSignInHandler() {
-      try {
-        const { token } = await authService.signIn(this.userCredentials);
-        sessionService.save(token);
-        this.$router.push({ name: "welcome" });
-      } catch (error) {
-        console.error(error);
+      this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+        try {
+          const { token } = await authService.signIn(this.userCredentials);
+          sessionService.save(token);
+          this.$router.push({ name: "welcome" });
+        } catch (error) {
+          if (error.response.status === 400) {
+            this.serverErrorMessage = "Invalid email or password";
+          } else {
+            this.serverErrorMessage =
+              "Something unexpected happened. Please try again";
+          }
+          console.error(error);
+        }
       }
     },
   },
@@ -72,6 +116,14 @@ export default {
 
   form {
     text-align: left;
+  }
+
+  .server-error-message {
+    width: 100%;
+    margin-top: 0.25rem;
+    font-size: 0.875em;
+    color: #dc3545;
+    margin-bottom: 1rem;
   }
 }
 </style>
