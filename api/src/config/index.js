04398a4a4215bc9ea842220c946773e89d96ded2
Patrick Schuab Gazaneo <pck1993@gmail.com>
2021-07-12 04:20:04 -0300

    Comment on config

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -3,6 +3,7 @@ module.exports = {
 	mongodbUrl: process.env.MONGODB_URL || "mongodb://localhost:27017",
 	passwordSaltRounds: 10,
 	jwt: {
+		//In a real app, this info would be retrieved in a secret service, like a vault service
 		secretKey: "feroot-challenge-key",
 		expires: "1 day",
 	},
