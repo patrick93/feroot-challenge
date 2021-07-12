dfbff2d4984eb5470103cc554d2c95f462a8c884
Patrick Gazaneo <pck1993@gmail.com>
2021-07-12 03:00:14 -0300

    add logout test on welcome page tests

--- ui/tests/unit/Welcome.spec.js
+++ ui/tests/unit/Welcome.spec.js
@@ -1,7 +1,12 @@
 import { shallowMount } from "@vue/test-utils";
+
 import userService from "../../src/services/user.service";
+import authService from "../../src/services/auth.service";
+
 import Welcome from "../../src/views/Welcome.vue";
 
+
+jest.mock("../../src/services/auth.service");
 jest.mock("../../src/services/user.service");
 
 describe("welcome page", () => {
@@ -28,6 +33,41 @@ describe("welcome page", () => {
     it("should render logout button", () => {
       expect(wrapper.find("button").exists()).toBeTruthy();
       expect(wrapper.find("button").text()).toEqual("Logout");
+    });
+  });
+
+  describe("when click on logout button", () => {
+    const userData = {
+      name: "User Name",
+      email: "user@email.com",
+    };
+
+    userService.getUserInfo.mockReset();
+    userService.getUserInfo.mockResolvedValue(userData);
+
+    authService.logout.mockReset();
+    authService.logout = jest.fn();
+
+    const router = {
+      push: jest.fn()
+    }
+
+    const wrapper = shallowMount(Welcome, {
+      mocks: {
+        $router: router
+      }
+    });
+
+    beforeEach(async () => {
+      await wrapper.find("button").trigger("click");
+    })
+
+    it("should call logout on authService", () => {
+      expect(authService.logout).toHaveBeenCalledTimes(1);
+    });
+
+    it("should redirect to sign in page", () => {
+      expect(router.push).toHaveBeenCalledWith({ name: "sign-in" });
     });
   });
 
